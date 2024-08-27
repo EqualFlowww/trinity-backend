fa7c01477ec73b6e755bd141e6fb9165a71c3cca
jzide <jzide@JzWork>
2024-08-27 11:59:16 +0900

    update

--- temp-data-checker.py
+++ temp-data-checker.py
@@ -13,7 +13,7 @@ import requests
 #===============================================================================
 # Implement
 #===============================================================================
-baseUrl = 'https://trinity.eqpls.net'
+baseUrl = 'https://trinity.dev.local'
 token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJkOHlPemhaSkJHU0ZUU3FBY2ZEYmpCM0pzOXpXSjFRRGJtTEhKclVEWVpRIn0.eyJleHAiOjE3MjQxNDk4OTgsImlhdCI6MTcyNDE0ODA5OCwiYXV0aF90aW1lIjoxNzI0MTQ4MDk4LCJqdGkiOiIyOWE5Y2YxZi03ODA4LTRkMmUtOTYwOS0wOThlMWMxYzQxZjYiLCJpc3MiOiJodHRwczovL3RyaW5pdHkuZXFwbHMubmV0L2F1dGgvcmVhbG1zL3RyaW5pdHkiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiYjZhNDM1NTgtNTU5MC00ODliLTg3ZWItZTNkNWUxY2JiYjFjIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoidHJpbml0eSIsInNpZCI6ImE2OTU0OGViLWY0ZDAtNDE5Ni1iYzE0LTJiNDA2ZGE1NjNhZSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXRyaW5pdHkiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgb3BlbmlkLWNsaWVudC1zY29wZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6ImFkbWluIGFkbWluIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW4iLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJmYW1pbHlfbmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkB0cmluaXR5LmVxcGxzLm5ldCIsInBvbGljeSI6WyJhZG1pbiIsInVzZXIiXX0.enfetsSgEk5BUPn6ofaMyQMB3CNts3BiHV9kegpYsAqlhY93jteIvRqiEXbSD5Q0mAL9wRrYiN1VzIB6TWDrZV2XKHKu3W6-6r5963IC-10iyxNAZ_4zqHTQojWH7ydEllCBFJ1N1TciZXGR9Thr9rc3Zr0fkSsEjjJXwREjcsMX-vJzh_XsUppj4ICyll5cvR_a2kcTneX5PVLcHiSzqoAvnySI9rGJkP8boHUaxOVI7mAHA61p-GF5hfj19_FrB2NLZs51B3k9D6inrfrNBo6uOElDiCFBLJ9S8IjgqM_Xg5UHAygVBNPlStRp9TBS3rDljY_kSMUs73jHDupK0g'
 
 headers = {
--- temp-data-loader.py
+++ temp-data-loader.py
@@ -14,7 +14,7 @@ import requests
 #===============================================================================
 # Implement
 #===============================================================================
-baseUrl = 'https://trinity.eqpls.net'
+baseUrl = 'https://trinity.dev.local'
 token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJreXdzeEVrc3ZWT296RDR4dmZlY2I0amF3R2tyYTlicm5hU1Y4cHNMdFdvIn0.eyJleHAiOjE3MjQyMjU0MjEsImlhdCI6MTcyNDIyMzYyMSwiYXV0aF90aW1lIjoxNzI0MjIzNjIxLCJqdGkiOiIwZmE2MzU0Yi1iY2RkLTRhZGQtODU0MC0wNmJlOTI2M2M5NGYiLCJpc3MiOiJodHRwczovL3RyaW5pdHkuZXFwbHMubmV0L2F1dGgvcmVhbG1zL3RyaW5pdHkiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiZjZiOGVhZjEtZTkxZi00MjZiLTlkNzctZmE5YmYzMzE0YTk5IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoidHJpbml0eSIsInNpZCI6IjJjNjZiNzZhLTZmNzEtNGY0Mi1hNjM5LTc1MTk5MTU0NzhhNiIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXRyaW5pdHkiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgb3BlbmlkLWNsaWVudC1zY29wZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6ImFkbWluIGFkbWluIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW4iLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJmYW1pbHlfbmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkB0cmluaXR5LmVxcGxzLm5ldCIsInBvbGljeSI6WyJhZG1pbiIsInVzZXIiXX0.CBnGJ3TIT6Zcgzxd0tNMwYsnJuWJg6GjlZqNbIo91AyNuPEakqzCbrrLoCJ4hBmd2M_5Vr3520EdMszsWsrublceYFtRCSGNrfYGQGfBlxFiseonIjeD4wqy0jgFHG-roGpchM-jQAaWWQXuL7CwxL5JEJXWfMzEEVz3zfAP3lWUeq-7JHm5gDixVKApEF-MP8ts_N0cJ-UpwGYQwewFZ2aN0U5Klp0CX6yq83nlVa8WRt3vlwAn7VUV1li9mMCHOvRNsHzph6y1myyUWO78YFbK3XVFmPFZ8k3KzVRNhkj8eA54grE6BvRe6mLfV_3oHsHKtx0YkXnx1G5xnQetRQ'
 
 headers = {
